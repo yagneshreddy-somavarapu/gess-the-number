6fae1bcc83f08cd049bf9d3cb54394a5f692f481
YagneshReddy.Somavarapu <84434246+yagneshreddy-somavarapu@users.noreply.github.com>
2025-03-28 17:59:37 +0530

    Update guss the number.py

--- guss the number.py
+++ guss the number.py
@@ -3,14 +3,17 @@ rand = rn.randint(1,25)
 count = 0
 while True:
    count += 1
-   print("Guess the number in between 1 to 25")
-   num = int(input())
-   if num == rand:
-      print(f'congrats you Won on {count}\'th attempt.')
-      break
-   elif rand > num:
-      print("To low")
-   elif rand < num:
-      print("To high")
-   else:
-      print("INVALID INPUT")
+   try :
+        print("Guess the number in between 1 to 25")
+        num = int(input())
+        if num == rand:
+           print(f'congrats you Won on {count}\'th attempt.')
+           break
+        elif rand > num:
+           print("To low")
+        elif rand < num:
+           print("To high")
+   except ValueError:
+      print("lease Enter Valid Number")
+     
+   
